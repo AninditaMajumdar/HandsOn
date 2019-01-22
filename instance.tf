1468ee70da36ad03d46d8d10d3cce33a60a044ba
AninditaMajumdar <43988527+AninditaMajumdar@users.noreply.github.com>
2019-01-22 11:38:45 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,5 +1,7 @@
 provider "aws" {
    region ="ap-southeast-1"
+   access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
 resource "aws_instance" "inst" {
   ami = "ami-daad67b9"
